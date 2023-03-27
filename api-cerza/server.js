f464dfd19862cc260ca9640de6d1fbc6263d0418
Hugo Poquet <hugo.poquet@icloud.com>
2023-03-27 15:49:53 +0200

    <feat>(BACK): API de base générée

--- api-cerza/server.js
+++ api-cerza/server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
-require("./app/routes/tutorial.routes.js")(app);
+require("./routes/user.routes.js")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
